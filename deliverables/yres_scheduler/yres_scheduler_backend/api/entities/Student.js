b349f98180fa5dc4621df7369b3638724511b839
Eric Karpovits <erickarpovits@gmail.com>
2023-11-11 13:51:08 -0800

    Finished the student entity

--- deliverables/yres_scheduler/yres_scheduler_backend/api/entities/Student.js
+++ deliverables/yres_scheduler/yres_scheduler_backend/api/entities/Student.js
@@ -5,49 +5,21 @@ module.exports = class Student {
     /**
      * Create a new student profile.
      * 
-     * @param {string} sID - The unique ID of this student.
+     * @param {string} student_id - The unique ID of this student.
      * @param {string} lastname
      * @param {string} firstname 
      * @param {number} age 
-     * @param {string} gender 
-     * @param {string} campusID - The ID of the campus this student registers.
-     */
-    constructor (sID, lastname, firstname, age, gender, campusID) {
-        this.sID = sID;
+     * @param {string} sex 
+     * @param {string[]} friend_ids - An array of student IDs that this student prefers to work with.
+     * @param {string[]} enemy_ids - An array of student IDs that this student doesn't want to work with.
+    */
+    constructor(student_id, lastname, firstname, age, sex, friend_ids, enemy_ids) {
+        this.student_id = student_id;
         this.lastname = lastname;
         this.firstname = firstname;
         this.age = age;
-        this.gender = gender;
-        this.campusID = campusID;
-    }
-
-    /**
-    * Dummy database used for testing.
-    */
-    static students = [
-        {
-            sID: "st00001",
-            lastname: "Dummy",
-            firstname: "Student 1",
-            age: 9,
-            gender: "M",
-            campusID: "CA00001"
-        },
-        {
-            sID: "st00002",
-            lastname: "Dummy",
-            firstname: "Student 2",
-            age: 10,
-            gender: "F",
-            campusID: "CA00001"
-        },
-        {
-            sID: "st00004",
-            lastname: "Dummy",
-            firstname: "Student 4",
-            age: 12,
-            gender: "M",
-            campusID: "CA00001"
-        }
-    ];
+        this.sex = sex;
+        this.friend_ids = friend_ids;
+        this.enemy_ids = enemy_ids;
+      }
 }
