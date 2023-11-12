f2a0b12728ee855c528b5def63dc34b9fe657030
Lanth139 <pf.zhang@mail.utoronto.ca>
2023-11-11 23:28:12 -0500

    add Room get request, change AdminUser request

--- deliverables/yres_scheduler/yres_scheduler_backend/api/services/accountService.js
+++ deliverables/yres_scheduler/yres_scheduler_backend/api/services/accountService.js
@@ -15,7 +15,6 @@ function login(username, password) {
         login_status: true,
         message: "Login successful"
     }
-
 }
 
 function signup(username, password) {
@@ -28,7 +27,7 @@ function signup(username, password) {
     }
     else {
         const userID = crypto.randomUUID();
-        db.newUser(userID, username, password);
+        db.createAdminUser(username, password);
         return {
             signup_status: true,
             userID: userID,
@@ -37,7 +36,7 @@ function signup(username, password) {
     }
 }
 
-function logout(userID) {
+function logout(username) {
     return {
         login_status: false,
         message: "Loggout successful"
