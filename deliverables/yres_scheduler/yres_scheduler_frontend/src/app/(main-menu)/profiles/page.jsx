d10195319386969d347f2f8d2e312172be4db276
Gabriel Anover <gabino6821@gmail.com>
2023-11-12 02:44:58 -0500

    Fixed state bug and refactored tables

--- deliverables/yres_scheduler/yres_scheduler_frontend/src/app/(main-menu)/profiles/page.jsx
+++ deliverables/yres_scheduler/yres_scheduler_frontend/src/app/(main-menu)/profiles/page.jsx
@@ -1,78 +1,89 @@
 'use client';
 import * as React from 'react';
-import ProfilesTable from "@/app/components/profilesTable";
-import DropdownButton from 'react-bootstrap/Dropdown';
-import Dropdown from 'react-bootstrap/Dropdown';
+import StudentProfilesTable from '@/app/components/studentProfilesTable';
+import CounselorProfilesTable from '@/app/components/counselorProfilesTable';
 import Button from 'react-bootstrap/Button';
-import RefinedDropdown from '../../components/refinedDropDowns'
-import StudentAdd from '../../modals/studentAdd'
+import RefinedDropdown from '@/app/components/refinedDropDowns';
+import StudentAdd from '@/app/modals/studentAdd'
 import { useState, useEffect } from 'react';
 
 const PROFILE_TYPES = ['Student', 'Counselor']
 
+const DUMMY_STUDENT_DATA = [{
+    student_id: 0,
+    firstname: 'Tom',
+    lastname: 'Bombadil',
+    age: '12',
+    sex: 'Male',
+    friends_ids: ['1'],
+    enemy_ids: ['2', '3']
+}, {
+    student_id: 1,
+    firstname: 'Jack',
+    lastname: 'Frost',
+    age: 7,
+    sex: 'Male',
+    friends_ids: ['0'],
+    enemy_ids: []
+},{
+    student_id: 2,
+    firstname: 'George',
+    lastname: 'Washington',
+    age: 8,
+    sex: 'Male',
+    friends_ids: ['3'],
+    enemy_ids: ['1']
+}, {
+    student_id: 3,
+    firstname: 'Abraham',
+    lastname: 'Lincoln',
+    age: 7,
+    sex: 'Male',
+    friends_ids: ['2'],
+    enemy_ids: ['1']
+}]
+
+const DUMMY_COUNSELOR_DATA = [{
+    counselor_id: 0,
+    firstname: 'Walter',
+    lastname: 'White',
+    campus_id: '11',
+}, {
+    counselor_id: 1,
+    firstname: 'Hank',
+    lastname: 'Schrader',
+    campus_id: '11',
+},{
+    counselor_id: 2,
+    firstname: 'Skylar',
+    lastname: 'White',
+    campus_id: '12',
+}, {
+    counselor_id: 3,
+    firstname: 'Jesse',
+    lastname: 'Pinkman',
+    campus_id: '12',
+}]
+
 function Profiles() {
     const [currType, setCurrType] = React.useState(PROFILE_TYPES[0]);
+    const [profileData, setProfileData] = useState([]);
     const [item, setItem] = useState({});
 
     const handleSelectType = (e) => {
         setCurrType(e);
     }
 
-    // Dummy state data. TODO: Replace with GET data api call
-    const [studentData, setStudentData] = useState([{
-        student_id: 0,
-        firstname: 'Tom',
-        lastname: 'Bombadil',
-        age: '12',
-        sex: 'Male',
-        friends_ids: ['1'],
-        enemy_ids: ['2', '3']
-    }, {
-        student_id: 1,
-        firstname: 'Jack',
-        lastname: 'Frost',
-        age: 7,
-        sex: 'Male',
-        friends_ids: ['0'],
-        enemy_ids: []
-    },{
-        student_id: 2,
-        firstname: 'George',
-        lastname: 'Washington',
-        age: 8,
-        sex: 'Male',
-        friends_ids: ['3'],
-        enemy_ids: ['1']
-    }, {
-        student_id: 3,
-        firstname: 'Abraham',
-        lastname: 'Lincoln',
-        age: 7,
-        sex: 'Male',
-        friends_ids: ['2'],
-        enemy_ids: ['1']
-    }]);
-    const [counselorData, setCounselorData] = useState([{
-        counselor_id: 0,
-        firstname: 'Walter',
-        lastname: 'White',
-        campus_id: '11',
-    }, {
-        counselor_id: 1,
-        firstname: 'Hank',
-        lastname: 'Schrader',
-        campus_id: '11',
-    },{
-        counselor_id: 2,
-        firstname: 'Skylar',
-        lastname: 'White',
-        campus_id: '12',
-    }, {
-        counselor_id: 3,
-        firstname: 'Jesse',
-        lastname: 'Pinkman',
-        campus_id: '12',
-    }]);
+    useEffect(() => {
+        if (currType === PROFILE_TYPES[0]) {
+            // TODO: Make get students API Request
+            setProfileData(DUMMY_STUDENT_DATA);
+        } else {
+            // TODO: Make get counselors API Request
+            setProfileData(DUMMY_COUNSELOR_DATA);
+        }
+    }, [currType]);
+    
     const [show, setShow] = useState(false);
     const handleShow = () => {
         const object = {
@@ -85,8 +96,8 @@ function Profiles() {
             enemy_ids: ['1']
         };
         setItem(object);
-        const addedArray = [...studentData, object];
-        setStudentData(addedArray);
+        const addedArray = [...profileData, object];
+        setProfileData(addedArray);
         setShow(true);
     };
     return (
@@ -102,15 +113,16 @@ function Profiles() {
                     <StudentAdd
                         show={show}
                         setShow={setShow}
-                        setStudentData={setStudentData} 
                         item={item}
                     />
                 </div>
             </div>
             <div className='center-align'>
-                <ProfilesTable type={currType} defaultType={PROFILE_TYPES[0]} 
-                studentData={studentData} setStudentData={setStudentData} 
-                counselorData={counselorData} setCounselorData={setCounselorData}/>
+                <div id='profiles-table'>
+                    {currType === PROFILE_TYPES[0] ? 
+                        <StudentProfilesTable studentData={profileData}/> : 
+                        <CounselorProfilesTable counselorData={profileData}/>}
+                </div>
             </div>
         </>
     );
---
+++ deliverables/yres_scheduler/yres_scheduler_frontend/src/app/components/studentProfilesTable.jsx
@@ -0,0 +1,82 @@
+import Button from 'react-bootstrap/Button';
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
+import Tooltip from 'react-bootstrap/Tooltip';
+import YresTable from './table'
+import StudentEdit from '../modals/studentEdit';
+import { FaPencilAlt } from 'react-icons/fa';
+import { BsTrash } from 'react-icons/bs';
+import { useState } from 'react';
+
+function StudentProfilesTable({ studentData, rowEvents }) {
+    const [showEdit, setShowEdit] = useState(false);
+    const [editItem, setEditItem] = useState({
+        student_id: -1,
+        firstname: null,
+        lastname: null,
+        age: null,
+        sex: null,
+        friends_ids: [],
+        enemy_ids: []
+    });
+
+    const columns = [{
+        dataField: 'student_id',
+        text: 'ID'
+    },{
+        dataField: 'firstname',
+        text: 'First Name'
+    },{
+        dataField: 'lastname',
+        text: 'Last Name'
+    },{
+        dataField: 'age',
+        text: 'Age'
+    },{
+        dataField: 'sex',
+        text: 'Sex'
+    },{
+        dataField: 'friends_ids',
+        text: 'Friends'
+    },{
+        dataField: 'enemy_ids',
+        text: 'Enemies'
+    },{
+        dataField: 'actions',
+        text: 'Actions'
+    }]
+
+    studentData.forEach(item => {
+        const showEditModal = () => {
+            setEditItem(item);
+            setShowEdit(true);
+        }
+        item.actions = (
+            <div className='table-actions'>
+                <OverlayTrigger placement="right-start" overlay={<Tooltip>View/Edit Student</Tooltip>}>
+                    <Button variant="success" onClick={showEditModal} className='action-button'>
+                        <FaPencilAlt />
+                    </Button>
+                </OverlayTrigger>
+                <OverlayTrigger placement="right-start" overlay={<Tooltip>Delete Student</Tooltip>}>
+                    <Button variant="danger" className='action-button'>
+                        <BsTrash />
+                    </Button>
+                </OverlayTrigger>
+            </div>
+        )
+    })
+
+    return (
+        <>
+            <YresTable keyCol={'student_id'} data={studentData} columns={columns} rowEvents={ rowEvents } disableHover={true}/>
+            <StudentEdit
+                item={editItem}
+                show={showEdit}
+                setShow={setShowEdit}
+                type={'Student'}
+            />
+        </>
+    )   
+}
+
+export default StudentProfilesTable;
