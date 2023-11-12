c86555bb60f8bb9e5c210a849bde5f6066049194
Ewan Jordan <ewan.jordan@mail.utoronto.ca>
2023-11-12 17:05:06 -0500

    Irrelevant comments

--- deliverables/yres_scheduler/yres_scheduler_frontend/src/app/components/table.jsx
+++ deliverables/yres_scheduler/yres_scheduler_frontend/src/app/components/table.jsx
@@ -6,8 +6,6 @@ function YresTable(props) {
 
   const [hydrated, setHydrated] = React.useState(false);
 	React.useEffect(() => {
-		// This forces a rerender, so the date is rendered
-		// the second time but not the first
 		setHydrated(true);
 	}, []);
 	if (!hydrated) {
