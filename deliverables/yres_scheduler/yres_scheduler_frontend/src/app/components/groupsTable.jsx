d57ce4f519d182507b02641ed2f7f63068c1c093
maxXu0713 <max.xu.0713@gmail.com>
2023-11-15 13:32:46 -0500

    group table

---
+++ deliverables/yres_scheduler/yres_scheduler_frontend/src/app/components/groupsTable.jsx
@@ -0,0 +1,52 @@
+import Button from 'react-bootstrap/Button';
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
+import Tooltip from 'react-bootstrap/Tooltip';
+import YresTable from './table'
+import { useState, useEffect } from 'react';
+import { FaPencilAlt } from 'react-icons/fa';
+import { BsTrash } from 'react-icons/bs';
+import ActivityEdit from '../modals/activityEdit';
+
+/** 
+ * 
+}
+*/
+function GroupsTable({data}) {
+    const [showEdit, setShowEdit] = useState(false);
+    const [editItem, setEditItem] = useState({
+        activity_id: -1,
+        name: null,
+        duration: null,
+        room_ids: [],
+        type: null,
+        num_occurences: -1
+    });
+
+    const columns = [{
+        dataField: 'group_id',
+        text: 'ID'
+    },{
+        dataField: 'schedule_id',
+        text: 'Schedule'
+    },{
+        dataField: 'student_ids',
+        text: 'Students'
+    },{
+        dataField: 'counselor_ids',
+        text: 'Counselors'
+    }]
+    return (
+        <div>
+            <div className='half-table-container'>
+                <YresTable keyCol={'activity_id'} data={data} columns={columns} disableHover={true}/>
+            </div>
+            <ActivityEdit
+                item={editItem}
+                show={showEdit}
+                setShow={setShowEdit}
+            />
+        </div>
+    )
+}
+
+export default GroupsTable;
