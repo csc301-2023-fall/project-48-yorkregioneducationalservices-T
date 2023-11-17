171b68e38797cd9a436295e3609239a05e331097
harvey.donnelly <harveydonnelly404@gmail.com>
2023-11-16 22:46:38 -0500

    entitiy tests

--- deliverables/yres_scheduler/yres_scheduler_backend/api/tests/entities/campTest.js
+++ deliverables/yres_scheduler/yres_scheduler_backend/api/tests/entities/campTest.js
@@ -1,33 +1,34 @@
-// const Camp = require("../../entities/Camp")
-// var assert = require('assert');
+const Camp = require("../../entities/Camp")
+var assert = require('assert');
 
-// var sample_age_type = 0;
-// var sample_campID = "c000000001";
-// var sample = new Camp(sample_age_type, sample_campID)
-// describe('class Camp', function () {
-//   describe('constructor()', function () {
-//     it('constructor should return an object', function () {
-//       assert.notEqual(sample, undefined);
-//     });
-//     it('with attributes properly set', function () {
-//       assert.equal(sample.age_type, sample_age_type);
-//       assert.equal(sample.campID, sample_campID);
-//     });
-//     it('with sets defined but empty', function() {
-//       assert.notEqual(sample.students, undefined);
-//       assert.notEqual(sample.counselors, undefined);
-//       assert.equal(sample.students.size, 0);
-//       assert.equal(sample.counselors.size, 0);
-//     });
-//     it('get set size methods should return 0', function() {
-//       assert.equal(sample.numStudents(), 0);
-//       assert.equal(sample.numCounselors(), 0);
-//     });
-//     it('get set size methods should return 1 with new item added', function() {
-//       sample.addStudents("dummy");
-//       sample.addCounselor("dummy");
-//       assert.equal(sample.numStudents(), 1);
-//       assert.equal(sample.numCounselors(), 1);
-//     });
-//   });
-// });
+var sample_camp_id = "6cc76258-ef76-4cf0-91fa-046fa177626e";
+var sample_name = "Test Camp";
+var sample_activity_ids = new Set(["id1", "id2", "id3"]);
+var sample_campus_id = "85d5faea-6088-4901-a662-f9a14b231ba7"
+var sample = new Camp(
+    sample_camp_id,
+    sample_name,
+    sample_activity_ids,
+    sample_campus_id);
+
+describe('class Camp', function () {
+  describe('constructor()', function () {
+    it('constructor should return an object', function () {
+      assert.notEqual(sample, undefined);
+    });
+    it('with attributes properly set', function () {
+      assert.equal(sample.camp_id, sample_camp_id);
+      assert.equal(sample.name, sample_name);
+      assert.equal(sample.activity_ids, sample_activity_ids);
+      assert.equal(sample.campus_id, sample_campus_id);
+    });
+    it('Camp.addActivity() works correctly', function() {
+      sample.addActivity();
+      sample_activity_ids.add( "658ab6df-ba1f-4fad-8fbd-016412dce1f4");
+      assert.equal(sample.activity_ids, sample_activity_ids);
+    });
+    it('Camp.numActivities() works correctly', function() {
+      assert.equal(sample.numActivities(), sample_activity_ids.size);
+    });
+  });
+});
---
+++ deliverables/yres_scheduler/yres_scheduler_backend/api/tests/services/campServiceTest.js
@@ -0,0 +1,45 @@
+const campusService = require('../../services/campusService');
+const Camp = require('../../entities/Campus');
+const assert = require('assert');
+const db = require('../../db/psqlDbPlugin');
+
+var result;
+var campus_name = "My Test Campus";
+
+describe('campusService', () => {
+	describe('createCampus()',  () => {
+		describe('Campus creation reported as successful', () => {
+			it('should return true',  () => {
+				try {
+					result = campusService.createCampus(campus_name);
+				} catch(err) {
+					assert.ifError(err);
+				}
+				result.then((res) => {
+					assert.equal(res, true);
+				});
+			});
+		});
+	});
+	
+	describe('getAllCampuses()',  () => {
+		describe('Newly-created campus is registered',  () => {
+			try {
+				result = campusService.getAllCampuses();
+			} catch(err) {
+				assert.ifError(err);
+			}
+			result.then((result)=>{
+				result.then((res) => {
+					it('should be an array of campus objects', () => {
+						for (var i=0; i<res.length; i++) {
+							(res[i]).should.be.instanceof(Campus);
+						}
+					});
+				});
+			}).catch((err) => {
+				assert.ifError(err);
+			});
+		});
+	});
+});
