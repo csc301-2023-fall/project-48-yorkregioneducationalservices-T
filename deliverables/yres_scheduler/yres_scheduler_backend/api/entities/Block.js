aaaede00b13f2a1770956ab5ea1de3c7eefa07b5
Lanth139 <pf.zhang@amil.utoronto.ca>
2023-11-16 23:08:54 -0500

    rollback entities change

--- deliverables/yres_scheduler/yres_scheduler_backend/api/entities/Block.js
+++ deliverables/yres_scheduler/yres_scheduler_backend/api/entities/Block.js
@@ -1,4 +1,3 @@
-const Activity = require("../entities/Activity");
 /**
  * Class representing a schedule block unit.
  */
@@ -9,16 +8,48 @@ module.exports = class Block {
      * @param {string} block_id - The unique ID assigned by <CONTROLLER>. 
      * @param {string} schedule_id - The ID of the schedule this block is in.
      * @param {string} room_id - The ID of the room this block is held in.
-     * @param {Activity} activity - The activity object this block is holding.
-     * @param {number} day - The day index of this block.
-     * @param {number} time - The time index of this block.
+     * @param {string} activity_id - The ID of the activity this block is hosting.
+     * @param {Date} start_time - The starting moment of this block.
+     * @param {Date} end_time - The ending moment of this block.
      */
-    constructor(block_id, schedule_id, room_id, activity, day, time) {
+    constructor(block_id, schedule_id, room_id, activity_id, start_time, end_time) {
         this.block_id = block_id;
         this.schedule_id = schedule_id;
         this.room_id = room_id;
-        this.activity = activity;
-        this.day = day;
-        this.time = time;
+        this.activity_id = activity_id;
+        this.start_time = start_time;
+        this.end_time = end_time;
+    }
+
+    /**
+     * @returns start_time as a string.
+     */
+    getStartTime() {    
+        var start_hours = this.start_time.getHours();
+        var start_minutes = this.start_time.getMinutes();
+        var start;
+        if (start_minutes < 10) {
+            start = start_hours + ":0" + start_minutes;
+        } else {
+            start = start_hours + ":" + start_minutes;
+        }
+
+        return start;
+    }
+
+    /**
+     * @returns end_time as a string.
+     */
+    getEndTime() {    
+        var end_hours = this.end_time.getHours();
+        var end_minutes = this.end_time.getMinutes();
+        var end;
+        if (end_minutes < 10) {
+            end = end_hours + ":0" + end_minutes;
+        } else {
+            end = end_hours + ":" + end_minutes;
+        }
+        
+        return end;
     }
 }
