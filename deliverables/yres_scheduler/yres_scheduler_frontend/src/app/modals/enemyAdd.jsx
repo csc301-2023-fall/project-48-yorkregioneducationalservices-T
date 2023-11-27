f13704f22738c489e6785bf7af805a0bfe536383
Ewan Jordan <ewan.jordan@mail.utoronto.ca>
2023-11-27 17:53:08 -0500

    error checking friends/enemies

--- deliverables/yres_scheduler/yres_scheduler_frontend/src/app/modals/enemyAdd.jsx
+++ deliverables/yres_scheduler/yres_scheduler_frontend/src/app/modals/enemyAdd.jsx
@@ -2,11 +2,13 @@ import { useState } from 'react';
 import Modal from 'react-bootstrap/Modal';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+import Alert from 'react-bootstrap/Alert';
 import { process_comma_separated_text, fetchDataPOST } from '../helper';
 import { useRouter } from 'next/navigation';
 import FriendSearchTable from '../components/friendSearchTable';
 function EnemiesCreate({show, setShow, studentData}) {
     const router = useRouter();
+    const [errorDisplay, setErrorDisplay] = useState(<></>);
     const [enemiesList, setEnemiesList] = useState([]);
     const handleClose = () => {
         setShow(false);
@@ -16,36 +18,51 @@ function EnemiesCreate({show, setShow, studentData}) {
         event.preventDefault();
         
         if(enemiesList.length < 2){
-            //throw error
-            
+            setErrorDisplay(<Alert variant="danger" onClose={() => setErrorDisplay(<></>)} dismissible>
+            <p>Please add at least two students to the enemy group.</p>
+            </Alert>)
         }
         else{
-        for(let i = 0; i < enemiesList.length - 1; i++){
-            for(let j = i + 1; j < enemiesList.length; j++){
-                try {
-                    await fetchDataPOST(
-                        "/students/createFriends/",
-                        {   
-                            student_id: enemiesList[i]._student_id,
-                            other_student_ui_id: enemiesList[j]._student_ui_id,
-                            enemy: true
+            let erred = false;
+            for(let i = 0; i < enemiesList.length - 1; i++){
+                for(let j = i + 1; j < enemiesList.length; j++){
+                    if(!studentData.find((student) => student._student_id === enemiesList[i]._student_id).enemy_ids.includes(enemiesList[j]._student_id)){
+                        try {
+                            await fetchDataPOST(
+                                "/students/createFriends/",
+                                {   
+                                    student_id: enemiesList[i]._student_id,
+                                    other_student_ui_id: enemiesList[j]._student_ui_id,
+                                    enemy: true
+                                }
+                            )
+                        } catch (err) {
+                            setErrorDisplay(<Alert variant="danger" onClose={() => setErrorDisplay(<></>)} dismissible>
+                            <p>{err}</p>
+                            </Alert>)
+                            erred = true;
                         }
-                    )
-                } catch (err) {
-                    //TODO: Display Error in component
-                    console.log(err);
+                    }
+                    else{
+                    setErrorDisplay(<Alert variant="danger" onClose={() => setErrorDisplay(<></>)} dismissible>
+                    <p>{enemiesList[i].firstname} and {enemiesList[j].firstname} are already enemies</p>
+                    </Alert>)
+                    erred = true;
+                    }
                 }
             }
+            if(!erred){
+                router.refresh();
+                handleClose();
+            }
         }
-        }
-        router.refresh();
-        handleClose();
     }
     return (
             <Modal show={show} onHide={handleClose}>
                 <Modal.Header closeButton>
                 <Modal.Title>{"Seperate Students"}</Modal.Title>
                 </Modal.Header>
+                {errorDisplay}
                 <Modal.Body>
                     <FriendSearchTable studentData={studentData} friends={enemiesList} setFriends={setEnemiesList} enemy={true}/> 
                     <Form onSubmit={handleSubmit}>
--- deliverables/yres_scheduler/yres_scheduler_frontend/src/app/modals/friendAdd.jsx
+++ deliverables/yres_scheduler/yres_scheduler_frontend/src/app/modals/friendAdd.jsx
@@ -2,49 +2,60 @@ import { useState } from 'react';
 import Modal from 'react-bootstrap/Modal';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+import Alert from 'react-bootstrap/Alert';
 import { process_comma_separated_text, fetchDataPOST } from '../helper';
 import { useRouter } from 'next/navigation';
 import FriendSearchTable from '../components/friendSearchTable';
 function FriendsCreate({show, setShow, studentData}) {
     const router = useRouter();
     const [friendsList, setFriendsList] = useState([]);
+    const [errorDisplay, setErrorDisplay] = useState(<></>);
     const handleClose = () => {
         setShow(false);
         setFriendsList([]);
     }
     const handleSubmit = async (event) => {
         event.preventDefault();
-        
         if(friendsList.length < 2){
-            //throw error
-            
+            setErrorDisplay(<Alert variant="danger" onClose={() => setErrorDisplay(<></>)} dismissible>
+            <p>Please add at least two students to friend group.</p>
+            </Alert>)
         }
-        else{
-            
-        for(let i = 0; i < friendsList.length - 1; i++){
-            for(let j = i + 1; j < friendsList.length; j++){
-                if(!studentData.find((student) => student._student_id === friendsList[i]._student_id).friend_ids.includes(friendsList[j]._student_id)){
-                    try {
-                        console.log(friendsList);
-                        await fetchDataPOST(
-                            "/students/createFriends/",
-                            {   
-                                student_id: friendsList[i]._student_id,
-                                other_student_ui_id: friendsList[j]._student_ui_id,
-                                enemy: false
-                            }
-                        )
-                    } catch (err) {
-                        //TODO: Display Error in component
-                        console.log(err);
+        else{    
+            let erred = false;
+            for(let i = 0; i < friendsList.length - 1; i++){
+                for(let j = i + 1; j < friendsList.length; j++){
+                    if(!studentData.find((student) => student._student_id === friendsList[i]._student_id).friend_ids.includes(friendsList[j]._student_id)){
+                        try {
+                            console.log(friendsList);
+                            await fetchDataPOST(
+                                "/students/createFriends/",
+                                {   
+                                    student_id: friendsList[i]._student_id,
+                                    other_student_ui_id: friendsList[j]._student_ui_id,
+                                    enemy: false
+                                }
+                            )
+                        } catch (err) {
+                            setErrorDisplay(<Alert variant="danger" onClose={() => setErrorDisplay(<></>)} dismissible>
+                            <p>{err}</p>
+                            </Alert>)
+                            erred = true;
+                        }
+                    }
+                    else{
+                        setErrorDisplay(<Alert variant="danger" onClose={() => setErrorDisplay(<></>)} dismissible>
+                        <p>{friendsList[i].firstname} and {friendsList[j].firstname} are already friends</p>
+                        </Alert>)
+                        erred = true;
                     }
                 }
             }
+            if(!erred){
+                router.refresh();
+                handleClose();
+            }
         }
-        }
-        router.refresh();
-        handleClose();
-
     }
     return (
         <>
@@ -52,6 +63,7 @@ function FriendsCreate({show, setShow, studentData}) {
                 <Modal.Header closeButton>
                 <Modal.Title>{"Create Friend Group"}</Modal.Title>
                 </Modal.Header>
+                {errorDisplay}
                 <Modal.Body>
                     <FriendSearchTable studentData={studentData} friends={friendsList} setFriends={setFriendsList}/> 
                     <Form onSubmit={handleSubmit}>
