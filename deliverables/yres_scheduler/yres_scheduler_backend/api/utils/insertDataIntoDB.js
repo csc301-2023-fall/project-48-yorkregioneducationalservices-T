8818ae5d441c9f0f4f6e78820e41b660431a783e
Eric Karpovits <erickarpovits@gmail.com>
2023-11-30 17:56:58 -0500

    Changes to the schedule table

--- deliverables/yres_scheduler/yres_scheduler_backend/api/utils/insertDataIntoDB.js
+++ deliverables/yres_scheduler/yres_scheduler_backend/api/utils/insertDataIntoDB.js
@@ -20,7 +20,7 @@ var counselor;
 var c1 = uuid.v1();
 var c2 = uuid.v1();
 for (let c = 1; c < 25; c++) {
-    await counselorDB.createCounselor('last', `first ${c}`);
+    await counselorDB.createCounselor(`first ${c}`, `last${c}`);
 }
 
 var lst = [];
@@ -34,9 +34,9 @@ for (let r = 0; r < 20; r++) {
 
 await activityDB.createActivity('Class 1', 1, 'common', 20, 1, `${lst[0]},${lst[1]},${lst[2]},${lst[3]},${lst[4]},${lst[5]},${lst[11]},${lst[12]},${lst[13]},${lst[14]},${lst[15]},${lst[16]},${lst[17]},${lst[18]},${lst[19]}`);
 // await activityDB.createActivity('Class 2', 1, 'common', 20, 'C2', `${lst[0]},${lst[1]},${lst[2]},${lst[3]},${lst[4]},${lst[5]},${lst[6]},${lst[7]},${lst[8]},${lst[9]},${lst[10]},${lst[11]},${lst[12]},${lst[13]},${lst[14]},${lst[15]},${lst[16]},${lst[17]},${lst[18]},${lst[19]}`);
-await activityDB.createActivity('Special 1', 2, 'common', 5, 1, `${lst[6]},${lst[7]},${lst[8]},${lst[9]},${lst[10]}`);
+await activityDB.createActivity('Gym', 2, 'common', 5, 1, `${lst[6]},${lst[7]},${lst[8]},${lst[9]},${lst[10]}`);
 // await activityDB.createActivity('Special 2', 2, 'common', 5, 'C2', `${lst[6]},${lst[7]},${lst[8]},${lst[9]},${lst[10]}`);
-await activityDB.createActivity('Other 1', 1, 'filler', 1, 1, `${lst[0]},${lst[1]},${lst[2]},${lst[3]},${lst[4]},${lst[5]},${lst[6]},${lst[7]},${lst[8]},${lst[9]},${lst[10]},${lst[11]},${lst[12]},${lst[13]},${lst[14]},${lst[15]},${lst[16]},${lst[17]},${lst[18]},${lst[19]}`);
+await activityDB.createActivity('Personal Study', 1, 'filler', 1, 1, `${lst[0]},${lst[1]},${lst[2]},${lst[3]},${lst[4]},${lst[5]},${lst[6]},${lst[7]},${lst[8]},${lst[9]},${lst[10]},${lst[11]},${lst[12]},${lst[13]},${lst[14]},${lst[15]},${lst[16]},${lst[17]},${lst[18]},${lst[19]}`);
 // await activityDB.createActivity('Other 2', 1, 'filler', 1, 'C2', `${lst[0]},${lst[1]},${lst[2]},${lst[3]},${lst[4]},${lst[5]},${lst[6]},${lst[7]},${lst[8]},${lst[9]},${lst[10]},${lst[11]},${lst[12]},${lst[13]},${lst[14]},${lst[15]},${lst[16]},${lst[17]},${lst[18]},${lst[19]}`);
 
 }
