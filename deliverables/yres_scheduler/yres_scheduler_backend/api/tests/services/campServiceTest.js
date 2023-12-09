62c3ade4d73b46edc7ec27385915c4447c144517
m-grigoriu <marcgrigoriu2003@gmail.com>
2023-12-09 18:11:45 -0500

    Database cleanup/documentation Addition

--- deliverables/yres_scheduler/yres_scheduler_backend/api/tests/services/campServiceTest.js
+++ deliverables/yres_scheduler/yres_scheduler_backend/api/tests/services/campServiceTest.js
@@ -1,45 +1,43 @@
-const campusService = require('../../services/campusService');
-const Camp = require('../../entities/Campus');
-const assert = require('assert');
-const db = require('../../db/psqlDbPlugin');
+// const campusService = require('../../services/campusService');
+// const Camp = require('../../entities/Campus');
+// const assert = require('assert');
+// var result;
+// var campus_name = "My Test Campus";
 
-var result;
-var campus_name = "My Test Campus";
-
-describe('campusService', () => {
-	describe('createCampus()',  () => {
-		describe('Campus creation reported as successful', () => {
-			it('should return true',  () => {
-				try {
-					result = campusService.createCampus(campus_name);
-				} catch(err) {
-					assert.ifError(err);
-				}
-				result.then((res) => {
-					assert.equal(res, true);
-				});
-			});
-		});
-	});
+// describe('campusService', () => {
+// 	describe('createCampus()',  () => {
+// 		describe('Campus creation reported as successful', () => {
+// 			it('should return true',  () => {
+// 				try {
+// 					result = campusService.createCampus(campus_name);
+// 				} catch(err) {
+// 					assert.ifError(err);
+// 				}
+// 				result.then((res) => {
+// 					assert.equal(res, true);
+// 				});
+// 			});
+// 		});
+// 	});
 	
-	describe('getAllCampuses()',  () => {
-		describe('Newly-created campus is registered',  () => {
-			try {
-				result = campusService.getAllCampuses();
-			} catch(err) {
-				assert.ifError(err);
-			}
-			result.then((result)=>{
-				result.then((res) => {
-					it('should be an array of campus objects', () => {
-						for (var i=0; i<res.length; i++) {
-							(res[i]).should.be.instanceof(Campus);
-						}
-					});
-				});
-			}).catch((err) => {
-				assert.ifError(err);
-			});
-		});
-	});
-});
+// 	describe('getAllCampuses()',  () => {
+// 		describe('Newly-created campus is registered',  () => {
+// 			try {
+// 				result = campusService.getAllCampuses();
+// 			} catch(err) {
+// 				assert.ifError(err);
+// 			}
+// 			result.then((result)=>{
+// 				result.then((res) => {
+// 					it('should be an array of campus objects', () => {
+// 						for (var i=0; i<res.length; i++) {
+// 							(res[i]).should.be.instanceof(Campus);
+// 						}
+// 					});
+// 				});
+// 			}).catch((err) => {
+// 				assert.ifError(err);
+// 			});
+// 		});
+// 	});
+// });
